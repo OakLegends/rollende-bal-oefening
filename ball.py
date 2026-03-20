dc6031b1759f43ebc3871f7342c55b621a654fb1
swinnen.noah <swinnen.noah@provilion.be>
2026-03-20 09:59:53 +0100

    3. the ball bounces on the walls.

--- ball.py
+++ ball.py
@@ -3,13 +3,17 @@ import pygame
 class Ball:
     def __init__(self, screen):
         self.screen = screen
-        self.x = 100                # startpositie links
+        self.x = 100
         self.y = screen.get_height() // 2
         self.radius = 50
-        self.speed = 5              # snelheid naar rechts
+        self.speed = 5              # positief = rechts, negatief = links
 
     def update(self):
-        self.x += self.speed        # bewegen naar rechts
+        self.x += self.speed
+
+        # Botsing rechterkant
+        if self.x + self.radius > self.screen.get_width():
+            self.speed = -self.speed   # richting omkeren
 
     def draw(self):
         pygame.draw.circle(self.screen, (255, 0, 0), (int(self.x), int(self.y)), self.radius)
